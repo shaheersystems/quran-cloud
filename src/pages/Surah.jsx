92ae73b4d9bed6a782e88cb1237e48bb6ddf1972
shaheerinit <105648279+shaheersystems@users.noreply.github.com>
2022-09-26 19:34:00 +0500

    some minor desgin changes

--- src/pages/Surah.jsx
+++ src/pages/Surah.jsx
@@ -21,7 +21,41 @@ function Surah() {
   console.log(data);
   return (
     <div className="flex flex-col justify-center items-center max-w-5xl m-auto max-h-fit py-10">
-      <h1 className="text-4xl font-arabic">{data.name}</h1>
+      <div className="h-16 w-full flex justify-end items-center gap-4 px-5">
+        <button className="p-2 rounded-full hover:bg-gray-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+            />
+          </svg>
+        </button>
+        <button className="p-2 rounded-full hover:bg-gray-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+            />
+          </svg>
+        </button>
+      </div>
+      <h1 className="text-4xl font-arabic py-5">{data.name}</h1>
       <div className="max-w-full max-h-fit py-10 gap-10 items-center flex flex-wrap flex-row-reverse justify-center border-b">
         {loading
           ? "Loading....."
